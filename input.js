527a2dc85409234133c5c29d7ffb53186d30b67e
96sMicks <melkiasae@hotmail.com>
2021-08-27 22:44:37 +0000

    added the keyboard commands to move the snake

--- input.js
+++ input.js
@@ -1,5 +1,7 @@
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,8 +11,26 @@ const setupInput = function () {
     if (key === '\u0003') {
       process.exit();
     }
+
+    if (key === 'w') {
+      connection.write("Move: up");
+    }
+
+    if (key === 'a') {
+      connection.write("Move: left");
+    }
+
+    if (key === 's') {
+      connection.write("Move: down");
+    }
+
+    if (key === 'd') {
+      connection.write("Move: right")
+    }
+
+
   };
-  
+
   stdin.on("data", handleUserInput);
   
 
